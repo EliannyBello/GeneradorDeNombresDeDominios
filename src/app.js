c87aa36704574ce457e374c00cc5fcca853a4241
EliannyBello <elibellomanzo@gmail.com>
2024-08-14 22:28:03 +0000

    comenzado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our", "us", "that"];
+let adj = ["great", "big", "cute", "little"];
+let noun = ["jogger", "racoon", "Bigger"];
+let dominio = [".com", ".es", ".net", ".us"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const nombreDominios = () => {
+  for (let i = 0; index < pronoun.length; index++) {
+    for (let a = 0; index < adj.length; index++) {
+      for (let n = 0; index < noun.length; index++) {
+        for (let d = 0; index < dominio.length; index++) {}
+      }
+    }
+  }
+  console.log(pronoun[i] + adj[a] + noun[n] + dominio[d]);
 };
+
+console.log(nombreDominios);
